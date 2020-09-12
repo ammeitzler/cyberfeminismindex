53790a3711ec128823786c01ec46d33f2da5ab7e
Angeline <ammeitzler@gmail.com>
2020-09-12 10:58:13 -0400

    media removed slash

--- cyberfem/settings/base.py
+++ cyberfem/settings/base.py
@@ -190,7 +190,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 
 # Wagtail settings
