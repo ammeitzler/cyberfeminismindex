36b925b66ae995d8f4644d18e0250b9e3fa03b77
Angeline <ammeitzler@gmail.com>
2020-09-12 11:06:20 -0400

    media removed slash

--- cyberfem/settings/base.py
+++ cyberfem/settings/base.py
@@ -189,7 +189,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = 'media/'
 
 
