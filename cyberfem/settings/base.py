df8175f3c37a32ece60d94ae07601abf38dc0bd3
Angeline <ammeitzler@gmail.com>
2020-09-12 11:13:19 -0400

    media removed slash

--- cyberfem/settings/base.py
+++ cyberfem/settings/base.py
@@ -189,8 +189,8 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Wagtail settings
--- cyberfem/settings/dev.py
+++ cyberfem/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
